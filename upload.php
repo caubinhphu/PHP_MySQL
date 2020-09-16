e3f5f3c94a648fd8bb42593a5e6e9f6ab4a7aee9
Anh Hai <10a3nguyenanhhai@gmail.com>
2020-09-16 09:29:31 +0700

    upload file

---
+++ upload.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="advanced.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="file" id="file" accept="image/*">
+    <div>
+      <input type="submit" value="Upload Image" name="submit">
+    </div>
+  </form>
+</body>
+</html>
